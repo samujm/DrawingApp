9df86ed7a23e81b5785e65c8bf2af2c89c3f7086
Samantha González <samujm76@gmail.com>
2022-09-11 19:27:13 -0500

    Funcionamiento para dibujar

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+
 const ctx = canvas.getContext("2d");
 
-let size = 20 
+let size = 10
+let isPressed = false
 let color = 'black'
 let x
 let y
+
+canvas.addEventListener('mousedown', (e)=>{
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+
+    // console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mouseup', (e)=>{
+    isPressed = false
+
+    x = undefined
+    y = undefined
+
+    // console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mousemove', (e)=>{
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+    
+        // console.log(x2, y2)
+        drawCircle(x2, y2)
+        drawLine(x,y,x2,y2)
+
+        x = x2
+        y = y2
+    } else {
+        
+    }
+})
 
 function drawCircle(x, y){
     ctx.beginPath();
@@ -20,9 +62,37 @@ function drawLine(x1, y1, x2, y2){
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
 
 // drawCircle(100, 200)
 // drawLine(300,300,200, 500)
+
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
